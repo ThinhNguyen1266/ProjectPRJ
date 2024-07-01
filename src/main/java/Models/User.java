22e6cce6799dc40c417bac237cf858a812d47336
DucNH <dantdm1243@gmail.com>
2024-07-01 21:09:29 +0700

    add model

---
+++ src/main/java/Models/User.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author DucNHCE180015
+ */
+public class User {
+    private int user_id;
+    private String name;
+    private int account_id;
+    private int phone_number;
+  
+}
